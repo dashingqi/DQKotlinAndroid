7a584bd71ce6d4a7d17d4dc11a7a962e2c84be50
DashingQi <dashingqi@gmail.com>
2021-02-23 17:36:08 +0800

    [update]

--- module-compose/src/main/java/com/dashingqi/module/compose/MainActivity.kt
+++ module-compose/src/main/java/com/dashingqi/module/compose/MainActivity.kt
@@ -3,30 +3,39 @@ package com.dashingqi.module.compose
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Space
+import androidx.compose.Composable
 import androidx.ui.core.Text
 import androidx.ui.core.dp
 import androidx.ui.core.setContent
+import androidx.ui.graphics.Image
 import androidx.ui.layout.Column
 import androidx.ui.layout.Row
 import androidx.ui.layout.Spacing
+import androidx.ui.res.imageResource
+import androidx.ui.tooling.preview.Preview
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(modifier = Spacing(left = 16.dp,top = 16.dp)) {
+            val image = imageResource(R.drawable.ic_launcher_background)
+            Column(modifier = Spacing(left = 16.dp, top = 16.dp)) {
                 Text("hello,this is dashingqi")
                 Text("哈哈哈哈哈，这就是我")
+
+
                 Row {
                     Text("this is me ")
                     Text("en，i know it")
                 }
-
-                Row() {
-
-                }
             }
         }
+    }
+
+    @Preview
+    @Composable
+    fun helloWorld() {
+        Text(text = "Hello World")
     }
 }
