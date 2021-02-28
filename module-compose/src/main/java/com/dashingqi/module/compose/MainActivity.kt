1f7d33e8f8fdbf19dfed7c9df8ba09614a3f7128
DashingQi <dashingqi@gmail.com>
2021-02-28 11:04:57 +0800

    [update]

--- module-compose/src/main/java/com/dashingqi/module/compose/MainActivity.kt
+++ module-compose/src/main/java/com/dashingqi/module/compose/MainActivity.kt
@@ -3,16 +3,14 @@ package com.dashingqi.module.compose
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Space
-import androidx.compose.Composable
-import androidx.ui.core.Text
-import androidx.ui.core.dp
-import androidx.ui.core.setContent
-import androidx.ui.graphics.Image
-import androidx.ui.layout.Column
-import androidx.ui.layout.Row
-import androidx.ui.layout.Spacing
-import androidx.ui.res.imageResource
-import androidx.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 
 class MainActivity : AppCompatActivity() {
@@ -20,7 +18,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             val image = imageResource(R.drawable.ic_launcher_background)
-            Column(modifier = Spacing(left = 16.dp, top = 16.dp)) {
+            Column{
                 Text("hello,this is dashingqi")
                 Text("哈哈哈哈哈，这就是我")
 
