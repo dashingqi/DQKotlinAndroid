66eb3b384e4183a7e02a50f55cfb56ded6444371
DashingQi <dashingqi@gmail.com>
2021-04-18 22:23:38 +0800

    [update]

---
+++ buildSrc/src/main/groovy/MyPlugin.groovy
@@ -0,0 +1,13 @@
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class MyPlugin implements Plugin<Project>{
+    @Override
+    void apply(Project project) {
+        project.task('CustomPluginTask') {
+            doLast {
+                println "自定义插件"
+            }
+        }
+    }
+}
