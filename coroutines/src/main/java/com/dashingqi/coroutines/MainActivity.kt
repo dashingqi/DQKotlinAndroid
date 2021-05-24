d2df76ddd2c65e22e4b2f8d2b6e2a26e9d7915ed
DashingQi <dashingqi@gmail.com>
2021-05-25 01:06:03 +0800

    [update]

--- coroutines/src/main/java/com/dashingqi/coroutines/MainActivity.kt
+++ coroutines/src/main/java/com/dashingqi/coroutines/MainActivity.kt
@@ -2,14 +2,13 @@ package com.dashingqi.coroutines
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import android.util.Log
+import kotlinx.coroutines.*
 import java.util.concurrent.Executors
 import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,6 +33,13 @@ class MainActivity : AppCompatActivity() {
             method1()
         }
 
+        GlobalScope.launch(Dispatchers.Main) {
+            val str = async { method2() }
+            val str2 = async { method3() }
+            handleResult(str.await(), str2.await())
+
+        }
+
 
     }
 
@@ -42,4 +48,19 @@ class MainActivity : AppCompatActivity() {
 
         }
     }
+
+    private suspend fun method2(): String {
+        delay(100)
+        return "nihao"
+    }
+
+    private suspend fun method3(): String {
+        delay(100)
+        return "zhangqi"
+    }
+
+    private fun handleResult(str: String, str2: String) {
+        Log.d(TAG, ":str = $str str2 = $str2 ")
+    }
+
 }
