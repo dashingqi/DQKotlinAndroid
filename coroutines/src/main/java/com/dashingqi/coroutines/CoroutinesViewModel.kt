c4965cdd8177a026d56ff1819d891446a460b5ff
DashingQi <dashingqi@gmail.com>
2021-05-25 00:24:51 +0800

    [update]

---
+++ coroutines/src/main/java/com/dashingqi/coroutines/CoroutinesViewModel.kt
@@ -0,0 +1,21 @@
+package com.dashingqi.coroutines
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+/**
+ * @author : zhangqi
+ * @time : 5/25/21
+ * desc :
+ */
+class CoroutinesViewModel(application: Application):AndroidViewModel(application) {
+
+
+    init {
+
+    }
+
+    fun test(){
+
+    }
+}
--- coroutines/src/main/java/com/dashingqi/coroutines/MainActivity.kt
+++ coroutines/src/main/java/com/dashingqi/coroutines/MainActivity.kt
@@ -2,11 +2,44 @@ package com.dashingqi.coroutines
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.util.concurrent.Executors
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Thread {}.start()
+        thread {
+
+        }
+        var newCachedThreadPool = Executors.newCachedThreadPool()
+        newCachedThreadPool.execute {
+
+        }
+
+
+
+        GlobalScope.launch {
+
+        }
+
+        GlobalScope.launch(Dispatchers.Main) {
+            method1()
+        }
+
+
+    }
+
+    private suspend fun method1() {
+        withContext(Dispatchers.IO) {
+
+        }
     }
 }
