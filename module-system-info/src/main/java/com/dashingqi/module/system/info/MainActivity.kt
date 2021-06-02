63c470fddf24b9ef6e69a615687d135c3737f53c
zhangqi61 <zhangqi61@baidu.com>
2021-06-02 21:28:28 +0800

    [update]

--- module-system-info/src/main/java/com/dashingqi/module/system/info/MainActivity.kt
+++ module-system-info/src/main/java/com/dashingqi/module/system/info/MainActivity.kt
@@ -2,14 +2,12 @@ package com.dashingqi.module.system.info
 
 import android.content.Context
 import android.net.ConnectivityManager
+import android.net.Network
 import android.net.NetworkRequest
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.telephony.CellInfo
-import android.telephony.PhoneStateListener
-import android.telephony.SubscriptionManager
-import android.telephony.TelephonyManager
+import android.telephony.*
 import android.util.Log
 
 class MainActivity : AppCompatActivity() {
@@ -18,33 +16,39 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val telephonyService =
-                getSystemService(Context.TELEPHONY_SERVICE)
+            getSystemService(Context.TELEPHONY_SERVICE)
         var subscriptionService = getSystemService(Context.TELEPHONY_SUBSCRIPTION_SERVICE)
         telephonyService?.let {
-//            val telephonyManager = it as TelephonyManager
-//            // 运营商的ID
-//
-//            var activeModemCount = telephonyManager.activeModemCount
-//
-//            var manufacturerCode = telephonyManager.manufacturerCode
+            val telephonyManager = it as TelephonyManager
+            val listener = object : PhoneStateListener() {
+                override fun onDataConnectionStateChanged(state: Int, networkType: Int) {
+                    super.onDataConnectionStateChanged(state, networkType)
+                    Log.d(TAG, "onDataConnectionStateChanged: ")
+                }
 
+                override fun onSignalStrengthsChanged(signalStrength: SignalStrength?) {
+                    super.onSignalStrengthsChanged(signalStrength)
+                    Log.d(TAG, "onDataConnectionStateChanged: ")
+                }
 
-//            Log.d(
-//                TAG,
-//                "simCarrierId = $simCarrierId " +
-//                        "cardIdForDefaultEuicc = $cardIdForDefaultEuicc" +
-//                        "activeModemCount = $activeModemCount" +
-//                        "manufacturerCode = $manufacturerCode"
-//            )
+            }
+
+//            telephonyManager.listen(listener, 0x00000100)
         }
 
         subscriptionService?.let {
             val subscriptionManager = it as SubscriptionManager
+            var accessibleSubscriptionInfoList = subscriptionManager.accessibleSubscriptionInfoList
+            Log.d(TAG, "onCreate: size =${accessibleSubscriptionInfoList?.size} ")
             var subscriptionClass = Class.forName(subscriptionManager.javaClass.name)
             var method = subscriptionClass.getMethod("getDefaultDataPhoneId")
             var method1 = subscriptionClass.getMethod("getDefaultDataSubscriptionId")
+            var method2 = subscriptionClass.getMethod("getActiveDataSubscriptionId")
+
             var invoke = method.invoke(subscriptionManager)
             var invoke1 = method1.invoke(subscriptionManager)
+            var invoke2 = method2.invoke(subscriptionManager)
+
 
             invoke?.let {
                 val code: Int = it as Int
@@ -55,16 +59,40 @@ class MainActivity : AppCompatActivity() {
                 val code1 = it as Int
                 Log.d(TAG, "code1 = $code1")
             }
+
+            invoke2?.let {
+                val code2 = it as Int
+                Log.d(TAG, "code2 = $code2")
+            }
+
         }
 
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             /** 获取到ConnectivityManager对象 */
-            val mConnectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as? ConnectivityManager
+            val mConnectivityManager =
+                getSystemService(Context.CONNECTIVITY_SERVICE) as? ConnectivityManager
+            Log.d(TAG, "perform ---->")
             mConnectivityManager?.let {
                 var builder =
-                        NetworkRequest.Builder()
+                    NetworkRequest.Builder()
                 var request = builder.build()
+                it.registerNetworkCallback(request, object : ConnectivityManager.NetworkCallback() {
+                    override fun onAvailable(network: Network) {
+                        super.onAvailable(network)
+                        Log.d(TAG, "onAvailable: ")
+                    }
+
+                    override fun onLosing(network: Network, maxMsToLive: Int) {
+                        super.onLosing(network, maxMsToLive)
+                        Log.d(TAG, "onLosing: ")
+                    }
+
+                    override fun onLost(network: Network) {
+                        super.onLost(network)
+                        Log.d(TAG, "onLost: ")
+                    }
+                })
             }
         }
     }
